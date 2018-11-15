4c4bef4afa65be30a0a22d557f497b8c7d9b5ad2
Hadagalberto Junior <hadagalberto.junior1@gmail.com>
2018-11-15 14:07:22 +0000

    publish config

--- PartyMaker/publish.sh
+++ PartyMaker/publish.sh
@@ -1,2 +1,11 @@
+cd ../
+git pull origin master
+cd PartyMaker
 rm -rf linux-publish/
 dotnet publish --configuration Release -r ubuntu.16.04-x64 -o linux-publish
+rm -rf linux-publish/appsett*
+killall screen
+cp -r linux-publish/* /var/www/partymaker
+chmod +x /var/www/partymaker/PartyMaker
+cd /var/www/partymaker/
+screen -d -m ./PartyMaker
