bf322c2bc034008ddbe34fcb9321ea792c8c6f92
alisongbsilva <alisongbsilva@gmail.com>
2018-11-27 21:13:02 -0200

    Update Register.cshtml.cs

--- PartyMaker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PartyMaker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,7 +70,7 @@ namespace PartyMaker.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("Usuário criou uma nova conta com senha.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.Page(
