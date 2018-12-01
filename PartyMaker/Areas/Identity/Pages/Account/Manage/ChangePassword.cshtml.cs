18ef82fbec7e7545cd94ca2102b54398731b83ba
alisongbsilva <alisongbsilva@gmail.com>
2018-11-30 23:35:06 -0200

    Algumas alterações e traduções

--- PartyMaker/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ PartyMaker/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -39,7 +39,7 @@ namespace PartyMaker.Areas.Identity.Pages.Account.Manage
             public string OldPassword { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Nova senha")]
             public string NewPassword { get; set; }
